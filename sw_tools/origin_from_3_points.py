edec6d9d91c3c3742610d0eb83f02da7810de4ea
bobby_robotics <bobbyradev99+github@gmail.com>
2023-04-13 17:18:14 +0200

    origin_from_3_points edited. Now working

--- sw_tools/origin_from_3_points.py
+++ sw_tools/origin_from_3_points.py
@@ -1,40 +1,45 @@
 import numpy as np
 
+# Input from 
+def point_input():
+
+    x = float(input("  x: "))
+    y = float(input("  y: "))
+    z = float(input("  z: "))
+
+    return np.array([x, y, z])
+
+
+def calc_origin(p1, p2, p3):
+    # Calculate the vector from p1 to p2
+    v1 = p2 - p1
+
+    # Calculate the vector from p1 to p3
+    v2 = p3 - p1
+
+    # Calculate the cross product of v1 and v2
+    v3 = np.cross(v1, v2)
+
+    # unit vectors
+    e_y = - v1 / np.linalg.norm(v1)
+    e_z = v3 / np.linalg.norm(v3)
+    e_x = np.cross(e_y, e_z)
+
+    lambda1 = ((p1[1] - p3[1])*e_x[0] + (p3[0] - p1[0])*e_x[1])/(e_x[1]*e_y[0] - e_x[0]*e_y[1])
+
+    origin = p1 + lambda1*e_y
+
+    return origin
+
+
 # Get the 3 points from the user
 print("Point 1:")
-x = float(input("  x: "))
-y = float(input("  y: "))
-z = float(input("  z: "))
-p1 = np.array([x, y, z])
+p1 = point_input()
 
 print("Point 2:")
-x = float(input("  x: "))
-y = float(input("  y: "))
-z = float(input("  z: "))
-p2 = np.array([x, y, z])
+p2 = point_input()
 
 print("Point 3:")
-x = float(input("  x: "))
-y = float(input("  y: "))
-z = float(input("  z: "))
-p3 = np.array([x, y, z])
+p3 = point_input()
 
-# Calculate the vector from p1 to p2
-v1 = p2 - p1
-
-# Calculate the vector from p1 to p3
-v2 = p3 - p1
-
-# Calculate the cross product of v1 and v2
-v3 = np.cross(v1, v2)
-
-# unit vectors
-e_x = v1 / np.linalg.norm(v1)
-e_z = v3 / np.linalg.norm(v3)
-e_y = np.cross(e_z, e_x)
-
-lambda1 = ((p1[1] - p3[1])*e_x[0] + (p3[0] - p1[0])*e_x[1])/(e_x[1]*e_y[0] - e_x[0]*e_y[1])
-
-origin = p1 + lambda1*e_x
-
-print("Origin: ", origin)
+print("Origin: ", calc_origin(p1, p2, p3))
