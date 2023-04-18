fixed world frame rotation direction

sw_tools/angles_to_plane.py
@@ -44,29 +44,39 @@ def find_plain_equation(A, B, C):
     fig = plt.figure()
     plt3d = fig.gca(projection='3d')
     plt3d.plot_surface(X, Y, Z)
+    plt.xlabel ="x"
+    plt.ylabel ="y"
+    plt.zlabel ="z"
     plt.show()
 
     z_ax = np.array([0, 0, 1])
 
     rot_axis = np.cross(z_ax, n)
 
-    rot_angle = acos( np.dot(z_ax,n) / ( np.linalg.norm(n) * np.linalg.norm(z_ax) ) )
+    rot_angle = -acos( np.dot(n,z_ax) / ( np.linalg.norm(n) * np.linalg.norm(z_ax) ) )
 
     print(rot_angle*180/(2*pi))
+
+    # normalize vector
+    rot_axis = rot_axis/np.linalg.norm(rot_axis)
+
+    q = [ cos(rot_angle/2), sin(rot_angle/2)*rot_axis ]
+
+    print(q)
 
     
 
 # Get the 3 points from the user
 print("Point 1:")
 #p0 = point_input()
-p0 = np.array([2 ,-2, 4.5])
+p0 = np.array([0.588193 , -0.0629043,  0.00409937])
 
 print("Point 2:")
 #p1 = point_input()
-p1 = np.array([-2, 3, 0])
+p1 = np.array([0.301129, -0.0612265, -0.00146244])
 
 print("Point 3:")
 #p2 = point_input()
-p2 = np.array([0, 3, -1.5])
+p2 = np.array([0.207729, 0.550682,0.00317771])
 
 find_plain_equation(p0,p1,p2)