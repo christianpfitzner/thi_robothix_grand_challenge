77b5080409b70b530cff807d8938cd2ca9617cd7
bobby_robotics <bobbyradev99+github@gmail.com>
2023-04-22 08:49:11 +0200

    added dependencies geometry_msgs, added exe for tf_tcp_probe, tf node for tcp to probe

---
+++ thi_robothix_grand_challenge/src/tf_tcp_probe_broadcaster.cpp
@@ -0,0 +1,23 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/tf.h>
+#include <geometry_msgs/Pose.h>
+
+
+void poseCallback(const geometry_msgs::Pose& msg){
+
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    br.sendTransform((msg.position.x, msg.position.y, msg.position.z),
+                     tf.transformations.quaternion_from_euler(0, 0, msg.orientation.z),
+                     ros::Time::now(),
+                     "panda_link_8",
+                     "panda_EE");
+}
+
+int main(int argc, char** argv){
+    
+    ros::init(argc, argv, "box_position_publisher_node");
+
+    return 0;
+};
